02cefdd92282cedceae60f8c57715d5d4f1c587d
sechiller <103475613+sechiller@users.noreply.github.com>
2022-06-25 21:39:44 -0500

    Add files via upload

---
+++ static/js/app.js
@@ -0,0 +1,10 @@
+// import the data from data.js
+const tableData = data;
+
+// Reference the HTML table using d3
+var tbody = d3.select("tbody");
+
+// Simple JavaScript console.log statement
+function printHello() {
+    console.log("Hello there!");
+  }
